fd5b9ce619f75d3aaf59182ee49839936c290720
Huaze726 <111575739+Huaze726@users.noreply.github.com>
2024-01-18 20:13:16 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = 0;
+rightWristX= 0;
+difference = 0;
+
 function preload() {}
 
 function setup() {
@@ -13,12 +17,20 @@ function modelLoaded() {
     console.log("PoseNet initialized");
 }
 
-function draw() {
-    background("gray");
-}
-
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + ", rightWristX = " + rightWristX + ", difference = " + difference);
     }
+}
+
+function draw() {
+    background("gray");
+    document.getElementById("font_size").innerHTML = "Font size of the text = " + difference + "px";
+    textSize(difference);
+    fill("white");
+    text("Huaze", 50,400);
 }
